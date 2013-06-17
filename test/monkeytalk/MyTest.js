4a9dcc9363f1150c071456eadbe75536527ef249
David Martin <david.martin@feedhenry.com>
2013-06-17 15:07:00 +0100

    Added app info to client html and updated test to collect this info

--- test/monkeytalk/MyTest.js
+++ test/monkeytalk/MyTest.js
@@ -21,15 +21,38 @@ MT.Application.prototype.myTest = function() {
 Test.MyTest.prototype.run = function() {
   var app = this.app;
 
+  app.debug().print("MyTest - start");
+
   // take a screenshot
+  app.debug().print("MyTest - screenshot");
   app.device().screenshot();
 
-  // verify the main div is visible
-  app.label('main').verify();
+  // verify title
+  app.debug().print("MyTest - verify title");
+  app.label('h1').verify();
+  app.label('h1').verify('Test App');
 
-  // verify text in main div
-  app.label('main').verify('Hello World');
+  // verify app info and build up info object
+  var info = {};
+  app.debug().print("MyTest - save info");
+  app.input('init').verify('ok');
+  app.input('destination').get('value');
+  app.input('app_version').get('value');
+  app.input('appid').get('value');
+  app.input('appkey').get('value');
+  app.input('host').get('value');
+  app.input('mode').get('value');
+  app.input('cuid').get('value');
+  app.input('agent').get('value');
+
+  app.debug().print("MyTest - make act call");
+  var req = '' + (new Date().getTime());
+  app.input('actreq').clear();
+  app.input('actreq').enterText(req);
+  app.label('makeactcall').tap();
+  app.input('act').verify('ok');
+  app.input('actres').verify(req);
 
   // some log output
-  app.debug().print("Finished MyTest");
+  app.debug().print("MyTest - end");
 };
